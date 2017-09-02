ae9c7f49444c9b38d201e2d587a0d4746c0940b5
izeh <31554814+izeh@users.noreply.github.com>
2017-09-02 09:11:22 -0500

    Update 1.r

--- 1.r
+++ 1.r
@@ -6,7 +6,7 @@ biling.survey = function(N = 40, pBi = .5, ...){
    
   par(las = 1, tck = -.02, font.lab = 2, cex.lab = 1, ...)  
   plot.ts(runProp, ty = "o", ylim = c(0, 1), yaxt = "n", pch = 21, bg = 3, xlab = "Number of Respondents", 
-          ylab = "Proportion of (B)", main = "Bilingual Education Survey")
+          ylab = "Proportion of (B)")
   
   axis(2, at = seq(0, 1L, len = 6), labels = paste0(seq(0, 1e2, len = 6), "%"))
   
