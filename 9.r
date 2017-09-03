7296219935591aef114a8f1368ad7f893bc56a41
izeh <31554814+izeh@users.noreply.github.com>
2017-09-03 09:28:07 -0500

    Update 9.r

--- 9.r
+++ 9.r
@@ -36,8 +36,7 @@ c(CI = CI.d(da = ds, n1a = n1, n2a = n2, conf.levela = conf.level), ds = ds)
   sim <- t(replicate(n.sim, fun()))
   capture = sim[ ,1] <= d & d <= sim[ ,2]
 
-  par(mgp = c(2, .2, 0), tck = -.015)  
-                
+  par(mgp = c(2, .2, 0), tck = -.015)    
   plot(sim[, 1:2], rep(1:n.sim, 2), ty = "n", ylab = NA, yaxt = "n", xlab = "Effect Size", font.lab = 2)
   axis(1, at = d, col.axis = 2, col = 2, font = 2)
   abline(h = 1:n.sim, col = 8, lty = 3)
