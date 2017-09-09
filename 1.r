fa2cf19871e2aee9414211f8b22d4d15bb2aea4f
izeh <31554814+izeh@users.noreply.github.com>
2017-09-09 11:05:11 -0500

    Update 1.r

--- 1.r
+++ 1.r
@@ -5,7 +5,7 @@ biling.survey = function(N = 40, pBi = .5, ...){
   Prop = cumsum(ResponseSequence) / 1:N   # Compute running proportion of B as each parent responds
    
   par(las = 1, tck = -.02, font.lab = 2, cex.lab = 1, ...)  
-  plot.ts(Prop, ty = "o", ylim = c(0, 1), yaxt = "n", pch = 21, bg = 3, xlab = "Number of Respondents", 
+  plot.ts(Prop, ty = "o", ylim = c(0, 1), yaxt = "n", pch = 21, bg = 3, xlab = "Number of Parents", 
           ylab = "Proportion of (B)")
   
   axis(2, at = seq(0, 1L, len = 6), lab = paste0(seq(0, 1e2, len = 6), "%"))
