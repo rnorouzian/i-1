619399a01bb8e41365d4775e21dd144a51fe0dd4
izeh <31554814+izeh@users.noreply.github.com>
2017-09-12 13:15:31 -0500

    Update 1.r

--- 1.r
+++ 1.r
@@ -1,7 +1,7 @@
 biling.survey = function(N = 40, pBi = .5, ...){
   
-  Responses = sample(x = 0:1, prob = c(1 - pBi, pBi), size = N, replace = TRUE)  # Generate N responses from parents
-                                                                                        # (B = 1, M = 0)
+  Responses = sample(0:1, N, prob = c(1 - pBi, pBi), replace = TRUE)  # Generate N responses from parents
+                                                                      # (B = 1, M = 0)
   Prop = cumsum(Responses) / 1:N   # Compute running proportion of B as each parent responds
    
   par(las = 1, tck = -.02, font.lab = 2, ...)  
@@ -10,9 +10,9 @@ biling.survey = function(N = 40, pBi = .5, ...){
   
   axis(2, at = seq(0, 1L, len = 6), lab = paste0(seq(0, 1e2, len = 6), "%"))
   
-  ResponseLetters = paste( c("M","B")[Responses[1L:1e1] + 1L], collapse = "")
+  ResponseSeq = paste(c("M", "B")[Responses[1L:1e1] + 1L], collapse = "")
   
-  Display = paste0("Response Sequence = ", ResponseLetters, ". . .")
+  Display = paste0("Response Sequence = ", ResponseSeq, ". . .")
   
   text(N, c(1, .95), c(Display, paste0("Proportion of B in ", N, " responses = ", Prop[N]*1e2, "%")), adj = c(1, .5), col = "red4", font = 2, cex = .8)
 }
