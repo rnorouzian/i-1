54df2e60b8570ef410341a4c096c62f8f0f646ac
izeh <31554814+izeh@users.noreply.github.com>
2017-10-04 10:08:23 -0500

    Update 7.r

--- 7.r
+++ 7.r
@@ -10,7 +10,7 @@ CI = HDI(posterior)
   
   axis(1, at = c(0, .2, .4, .7, .85, 1), labels = paste0(c(0, .2, .4, .7, .85, 1)*1e2, "%"), font = 2)
   mode = optimize(posterior, interval = c(0, 1), maximum = TRUE, tol = 1e-12)[[1]]
-  axis(1, at = mode, labels = paste0(round(mode, 4)*1e2, "%"), font = 2, col = 2, col.axis = 2 )
+  axis(1, at = mode, labels = paste0(round(mode, 4)*1e2, "%"), font = 2, col = 2, col.axis = 2)
   segments(mode, 0, mode, max(Posterior), lty = 3, col = "darkgreen")
 
   segments(CI[1], 0, CI[2], 0, lend = 1, lwd = 4, col = "magenta", xpd = NA)
