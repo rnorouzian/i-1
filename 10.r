2cc2afbe665007720d97925e9c4a48f01f513798
izeh <31554814+izeh@users.noreply.github.com>
2017-12-10 13:47:46 -0600

    Update 10.r

--- 10.r
+++ 10.r
@@ -1,25 +1,54 @@
-p.CI <- function(s, n, conf.level = .95) { ## s = no. of agreements
-                                           ## n = total no. of parents
+d.priors <- function(n1, n2 = NA, t, m, s, lo = -Inf, hi = Inf, dist.name = c("dnorm", "dcauchy", "dlogis"),
+                     scale = .35, margin = 7, top = .71){
+   
+   d = dist.name
+loop = length(d) 
+  CI = matrix(NA, loop, 2)
+mode = numeric(loop)
+mean = numeric(loop)
+  sd = numeric(loop)
+from = numeric(loop)
+  to = numeric(loop) 
+   h = list()
   
-  par(mfrow = c(2, 1), mar = c(3, 4.1, 3, .5), las = 1, lend = 1)
-  alpha = (1 - conf.level)/2
-  
-  L <- if(s == 0) 0 else qbeta(alpha, s, n - s + 1)
-  U <- if(s == n) 1 else qbeta(1 - alpha, s + 1, n - s)
-  
-  curve(dbeta(x, s, n - s + 1), n = 1e4, lwd = 2, col = 4, ylab = "Density", main = "Beta Distributions", xlab = NA)
-  curve(dbeta(x, s + 1, n - s), n = 1e4, lwd = 2, col = 2, add = TRUE)
-  
-  abline(v = c(L, U), col = c(4, 2), lty = 3)
-  
-  segments(L, 0, U, 0, lwd = 4) ; points(s/n, 0, pch = 19, cex = 2)
-  
-  text(c(L, U, s/n), 0, signif(c(L, U, s/n), 2), pos = 3, font = 2, xpd = TRUE)
-  
-  plot(x <- 0:n, dbinom(x, n, L), ty = "h", lwd = 2, col = 4, ylab = "Probability", main = "Binomial Distributions")
-  lines(x <- 0:n, dbinom(x, n, U), ty = "h", lwd = 2, col = 2, lty = 2, xpd = TRUE)
-  
-  return(c(L, U))
+ N = ifelse(is.na(n2), n1, n1 * n2 / (n1 + n2))
+df = ifelse(is.na(n2), n1 - 1, n1 + n2 - 2)   
+
+options(warn = -1)   
+original.par = par(no.readonly = TRUE)
+on.exit(par(original.par))
+   
+for(i in 1:loop){
+         p = function(x) get(d[i])(x, m, s)*as.integer(x >= lo)*as.integer(x <= hi)
+     prior = function(x) p(x)/integrate(p, lo, hi)[[1]]
+likelihood = function(x) dt(t, df, x*sqrt(N))
+         k = integrate(function(x) prior(x)*likelihood(x), lo, hi)[[1]]
+ posterior = function(x) prior(x)*likelihood(x) / k
+   mode[i] = optimize(posterior, c(-9, 9), maximum = TRUE, tol = 1e-20)[[1]]
+   mean[i] = integrate(function(x) x*posterior(x), lo, hi)[[1]]
+     sd[i] = sqrt(integrate(function(x) x^2*posterior(x), lo, hi)[[1]] - mean^2)
+    CI[i,] = HDI(posterior, -5, 5)
+   from[i] = mean - margin * sd
+     to[i] = mean + margin * sd  
+h[i] = list(curve(posterior, from, to, type = "n", ann = FALSE, yaxt = "n", xaxt = "n", add = i!= 1, bty = "n", n = 5e2))
 }
-#Example of use:
-p.CI(s = 55, n = 100)
+
+f = sapply(h, function(x) max(x[[2]])) + 1:loop
+par(mgp = c(2, .5, 0))                                             
+plot(CI[, 1:2], rep(1:loop, 2), type = "n", xlim = c(min(from), max(to)), ylim = c(1, top*max(f)), ylab = NA, yaxt = "n", xlab = "Credible Interval 'd'", font.lab = 2)
+abline(h = 1:loop, col = 8, lty = 3)
+segments(CI[, 1], 1:loop, CI[, 2], 1:loop, lend = 1, lwd = 4, col = 1:loop)
+axis(2, at = 1:loop, lab = d, font = 2, las = 1, cex.axis = .8, tick = FALSE) ; axis(3)
+
+for(i in 1:loop){
+  polygon(x = h[[i]]$x, y = scale*h[[i]]$y +i, col = adjustcolor(i, .4), border = NA, xpd = NA)
+}
+  a = scale*(f-1:loop)+1:loop
+  segments(mode, 1:loop, mode, a, lty = 3, xpd = NA, lend = 1)
+  points(mode, 1:loop, pch = 21, bg = "cyan", cex = 1.1, col = 4, xpd = NA)
+  deci = function(x, k = 3) format(round(x, k), nsmall = k)
+  I = deci(CI) ; o = deci(mode)
+  text(c(CI[,1], o, CI[,2]), 1:loop, c(I[,1], o, I[,2]), pos = 3, font = 2, cex = .8, xpd = NA)
+
+structure(list(post.mode = deci(mode, 6), post.mean = deci(mean, 6), post.sd = deci(sd, 6)), class = "power.htest")
+}
