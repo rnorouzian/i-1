1046aa5405f0f4fc6a232d7c3941092c2798c0f3
izeh <31554814+izeh@users.noreply.github.com>
2017-12-14 19:51:54 -0600

    Update i.r

--- i.r
+++ i.r
@@ -258,3 +258,66 @@ ab.prop.hyper <- function(a, b, lo = 0, hi = 1, dist.name, add = FALSE,
 
 #====================================================================================================================
 
+d.priors <- function(t, n1, n2 = NA, m, s, lo = -Inf, hi = Inf, dist.name, scale = 1, margin = 7, top = .8, show.prior = FALSE){
+  
+  d = dist.name ; pr = show.prior
+  is.eq = function(...) length(unique(lengths(list(...)))) == 1
+  ab.df = function(x, y) c(x, rep(rev(x)[1], abs(length(y) - length(x))))
+  
+  if(is.eq(m, s) & !is.eq(m, s, d)) d = ab.df(d, m) else if(is.eq(m, d) & !is.eq(m, s, d)) s = ab.df(s, m) else if(is.eq(s, d) & !is.eq(m, s, d)) m = ab.df(m, d)
+  
+  loop = length(d) 
+  CI = matrix(NA, loop, 2)
+  mode = numeric(loop)
+  mean = numeric(loop)
+  sd = numeric(loop)
+  from = numeric(loop)
+  to = numeric(loop) 
+  h = list()
+  
+  N = ifelse(is.na(n2), n1, n1 * n2 / (n1 + n2))
+  df = ifelse(is.na(n2), n1 - 1, n1 + n2 - 2)   
+  
+  options(warn = -1)
+  
+  for(i in 1:loop){
+    p = function(x) get(d[i])(x, m[i], s[i])*as.integer(x >= lo)*as.integer(x <= hi)
+    prior = function(x) p(x)/integrate(p, lo, hi)[[1]]
+    
+    if(!pr){     
+      likelihood = function(x) dt(t, df, x*sqrt(N))
+      k = integrate(function(x) prior(x)*likelihood(x), lo, hi)[[1]]
+      posterior = function(x) prior(x)*likelihood(x) / k
+      mean[i] = integrate(function(x) x*posterior(x), lo, hi)[[1]]
+      sd[i] = sqrt(integrate(function(x) x^2*posterior(x), lo, hi)[[1]] - mean^2)
+      from[i] = mean - margin * sd
+      to[i] = mean + margin * sd
+      mode[i] = optimize(posterior, c(from, to), maximum = TRUE, tol = 1e-10)[[1]]
+      CI[i,] = HDI(posterior, from, to)
+      h[i] = list(curve(posterior, from, to, type = "n", ann = FALSE, yaxt = "n", xaxt = "n", add = i!= 1, bty = "n", n = 5e2))
+    }
+  }
+  
+  if(!pr){
+    f = sapply(h, function(x) max(x[[2]])) + 1:loop
+    plot(CI[, 1:2], rep(1:loop, 2), type = "n", xlim = c(min(from), max(to)), ylim = c(1, top*max(f)), ylab = NA, yaxt = "n", xlab = "Credible Interval 'd'", font.lab = 2, mgp = c(2, .5, 0))
+    abline(h = 1:loop, col = 8, lty = 3)
+    legend("topleft", paste0(substring(d, 2), "(", round(m, 2), ", ", round(s, 2), ")"), pch = 22, pt.bg = 1:loop, col = 1:loop, cex = .7, bg = NA, bty = "n", pt.cex = .6)
+    segments(CI[, 1], 1:loop, CI[, 2], 1:loop, lend = 1, lwd = 4, col = 1:loop)
+    axis(2, at = 1:loop, lab = d, font = 2, las = 1, cex.axis = .8, tick = FALSE, mgp = c(2, .5, 0)) ; axis(3, mgp = c(2, .5, 0))
+    
+    for(i in 1:loop){
+      polygon(x = h[[i]]$x, y = scale*h[[i]]$y +i, col = adjustcolor(i, .4), border = NA, xpd = NA)
+    }
+    a = scale*(f-1:loop)+1:loop
+    segments(mode, 1:loop, mode, a, lty = 3, xpd = NA, lend = 1)
+    points(mode, 1:loop, pch = 21, bg = "cyan", cex = 1.1, col = 4, xpd = NA)
+    deci = function(x, k = 3) format(round(x, k), nsmall = k)
+    I = deci(CI) ; o = deci(mode)
+    text(c(CI[,1], o, CI[,2]), 1:loop, c(I[,1], o, I[,2]), pos = 3, font = 2, cex = .8, xpd = NA)
+  }else{
+    curve(prior, -6, 6, yaxt = "n", ylab = NA, xlab = bquote(bold("Effect Size "(delta))), bty = "n", font.lab = 2, lwd = 2, n = 1e3, main = bquote(delta*" ~ "*.(if(lo > -Inf || hi < Inf) "truncated-")*.(substring(d, 2))(.(round(m, 2)), .(round(s)))), mgp = c(2, .5, 0))
+  }
+}
+
+#========================================================================================================================
