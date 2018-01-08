cc754229dc04fb00375a330b81ebfd4ebcd10b10
izeh <31554814+izeh@users.noreply.github.com>
2018-01-07 18:10:27 -0600

    Update 9.r

--- 9.r
+++ 9.r
@@ -37,7 +37,7 @@ c(CI = CI.d(da = ds, n1a = n1, n2a = n2, conf.levela = conf.level), ds = ds)
   capture = sim[ ,1] <= d & d <= sim[ ,2]
 
   par(mgp = c(2, .2, 0), tck = -.015)    
-  plot(sim[, 1:2], rep(1:n.sim, 2), ty = "n", ylab = NA, yaxt = "n", xlab = "Effect Size (Cohen’s d)", font.lab = 2)
+  plot(sim[, 1:2], rep(1:n.sim, 2), ty = "n", ylab = NA, yaxt = "n", xlab = "Effect Size (Cohen's d)", font.lab = 2)
   axis(1, at = d, col.axis = 2, col = 2, font = 2)
   abline(h = 1:n.sim, col = 8, lty = 3)
   if(ylabel) axis(2, at = 1:n.sim, labels = paste0("Repeat ", rev(1:n.sim)), font = 2, las = 1, cex.axis = .8, tck = -.006)
