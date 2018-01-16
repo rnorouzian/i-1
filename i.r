215a6a0e2dc2809b3e774f8c3758fab525a97fbd
izeh <31554814+izeh@users.noreply.github.com>
2018-01-16 00:48:02 -0600

    Update i.r

--- i.r
+++ i.r
@@ -492,7 +492,7 @@ from <- numeric(loop)
 
 for(i in 1:loop){
      CI[i,] <- hdir(ps[, i], level = level)
-     den[i] <- list(density(ps[, i], adjust = 2, n = 1e3))
+   den[[i]] <- density(ps[, i], adjust = 2, n = 1e3)
      f[[i]] <- approxfun(den[[i]]$x, den[[i]]$y, yleft = 0, yright = 0)
        x[i] <- integrate(f[[i]], -1, pL)[[1]]
        y[i] <- integrate(f[[i]], -1, pU)[[1]]
@@ -513,7 +513,7 @@ for(i in 1:loop){
   plot(CI, rep(1:loop, 2), type = "n", xlim = c(min(from), max(to)), ylim = c(bottom*1, top*loop), ylab = NA, xaxt = "n", yaxt = "n", xlab = "Credible Interval (Proportion Differences)", font.lab = 2, mgp = c(2, .3, 0))
   axis(1, at = axTicks(1), labels = paste0(round(axTicks(1), 2)*1e2, "%"), mgp = c(2, .3, 0))
   abline(h = 1:loop, col = 8, lty = 3)
-  axis(2, at = 1:loop, labels = np, font = 2, las = 1, cex.axis = .8, tck = -.006, mgp = c(2, .3, 0))
+  axis(2, at = 1:loop, labels = np, font = 2, las = 1, cex.axis = .8, tck = -.006, mgp = c(2, .3, 0), cex.lab = .9)
   legend(legend, rep(rev(paste0("beta", "(", round(a, 2), ", ", round(b, 2), ")")), leg), pch = 22, title = "Priors", pt.bg = rep(loop:1, each = leg), col = rep(loop:1, each = leg), cex = .7, pt.cex = .6, bg = 0, box.col = 0, xpd = NA, x.intersp = .5, title.adj = .4)
   segments(CI[, 1], 1:loop, CI[, 2], 1:loop, lend = 1, lwd = 4, col = 1:loop, xpd = NA)
   box()
