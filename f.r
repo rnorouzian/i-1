9bb11f6018db4e55df2737885e6229c99529a143
izeh <31554814+izeh@users.noreply.github.com>
2018-03-06 15:34:41 -0600

    Update f.r

--- f.r
+++ f.r
@@ -8,7 +8,7 @@ pre.t = rnorm(100)
 pos.t = rnorm(100, 5)
 
 
-data = data.frame(subjects = rep(1:200, each = 2), 
+data.frame(subjects = rep(1:200, each = 2), 
                   y = c(pre.c, pos.c, pre.t, pos.t), 
                   time = rep(0:1, 200), 
                   group = rep(c(0, 1), each = 200))
