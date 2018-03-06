80c3e123200f26448e20a4d130bafbccc450e3e8
izeh <31554814+izeh@users.noreply.github.com>
2018-03-06 15:33:42 -0600

    Update f.r

--- f.r
+++ f.r
@@ -1,10 +1,14 @@
 
-foo <- function(x)
-{
-  UseMethod("foo")
-}
+pre.c = rnorm(100)
 
-foo.default <- function(x){
-  
-  return(c(x + 1, x - 1))
-}
+pos.c = rnorm(100)
+
+pre.t = rnorm(100)
+
+pos.t = rnorm(100, 5)
+
+
+data = data.frame(subjects = rep(1:200, each = 2), 
+                  y = c(pre.c, pos.c, pre.t, pos.t), 
+                  time = rep(0:1, 200), 
+                  group = rep(c(0, 1), each = 200))
