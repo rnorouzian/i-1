74dfcf1e36d7217ad97006f624de7040cf7779f7
izeh <31554814+izeh@users.noreply.github.com>
2018-04-07 12:03:28 -0500

    Update 1.r

--- 1.r
+++ 1.r
@@ -5,13 +5,14 @@ Responses = sample(0:1, size = N, prob = c(1 - pBi, pBi), replace = TRUE)  # Gen
 Prop = cumsum(Responses) / 1:N   # Compute running proportion of B as each parent responds
    
 par(las = 1, tck = -.02, font.lab = 2, ...)  
-plot.ts(Prop, type = "o", ylim = 0:1, yaxt = "n", pch = 21, bg = 3, xlab = "Number of Parents", 
-        ylab = "Proportion of (B)")
+plot.ts(Prop, type = "o", ylim = 0:1, yaxt = "n", pch = 21, bg = 3, xlab = "Parent ID number", 
+        ylab = "Proportion of (B)", xaxt = "n")
 
 CI = binom.test(cumsum(Responses)[N], N)[[4]]
 arrows(N, CI[1], N, CI[2], code = 3, angle = 90, length = .08, lend = 1)
 points(N, Prop[N], pch = 21, bg = "cyan")
-  
+ 
+axis(1, at = axTicks(1), lab = c(1, axTicks(1)[-1]))  
 axis(2, at = seq(0, 1L, len = 6), lab = paste0(seq(0, 1e2, len = 6), "%"))
   
 ResponseSeq = paste(c("M", "B")[Responses[1L:1e1] + 1L], collapse = "")
